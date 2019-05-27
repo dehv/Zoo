ccb4c6f58bd7f3d855f29606fd0eaa5d027ff843
$TH5J00-I3TNS7R3KADS <$TH5J00-I3TNS7R3KADS@LAB21-25.stud.hshl.net>
2019-05-27 14:16:33 +0200

    Tier Interface Controller weiter bearbeitet

--- ZooManager/src/zoomanager/TierInterfaceController.java
+++ ZooManager/src/zoomanager/TierInterfaceController.java
@@ -6,15 +6,23 @@
 package zoomanager;
 
 import java.net.URL;
+import java.time.LocalDate;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.MenuButton;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import zoomanager.Gehege;
+import zoomanager.Geschlecht;
+import zoomanager.Mitarbeiter;
+import zoomanager.ZooManager;
 
 /**
  * FXML Controller class
@@ -23,6 +31,8 @@ import javafx.scene.control.TextField;
  */
 public class TierInterfaceController implements Initializable {
 
+    public ZooManager main;
+    
     @FXML
     private TextField tierNameTextField;
     @FXML
@@ -41,13 +51,38 @@ public class TierInterfaceController implements Initializable {
     private Button tierFertigButton;
     @FXML
     private Button tierAbbrechenButton;
+    
+    void setMain(ZooManager zooManager) {
+        this.main = zooManager;
+    }
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+       if (checkInputs()) {
+        String art = tierArtTextField.getText();
+        String name = tierNameTextField.getText();
+        String idnummer = tierIdNummerTextField.getText();
+        String telefonnummer = mitarbeiterTelefonTextField.getText();
+        String personalnummer = mitarbeiterPersonalnummerTextField.getText();
+        //TODO beruf enum??
+        String beruf = "Noch nicht implementiert!";
+        LocalDate geburtstag = mitarbeiterGeburtsdatumDatePicker.getValue();
+        Geschlecht geschlecht = (Geschlecht) mitarbeiterGeschlechtChoiceBox.getValue();
+        main.addMitarbeiter(new Mitarbeiter(vorname, name, addresse, telefonnummer, beruf, geburtstag, geschlecht, personalnummer));
+        Stage stage = (Stage) mitarbeiterAbbrechenButton.getScene().getWindow();
+        stage.close();
+        }
+        else {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Fehlende Werte");
+            alert.setHeaderText(null);
+            alert.setContentText("Bitte geben Sie für alle Felder einen Wert ein und versuchen Sie es nochmal!");
+            
+            alert.showAndWait();
+        }
     }    
 
     @FXML
